abb681d242759e821fc062a853f497c34e1f465c
Dima_Odessa <dima_odessa@MacBook-2025-DimaOdessa.local>
2026-04-13 15:46:07 +0200

    Modified Atomic version

--- Assignment1b/2-histogram-atomic.cpp
+++ Assignment1b/2-histogram-atomic.cpp
@@ -4,53 +4,35 @@
 #include <chrono>
 #include <numeric>
 #include <thread>
+#include <mutex>
 #include <atomic>
 #include <cstring>
-#include <algorithm>
 #include "helper.hpp"
 
 using namespace std;
 
-struct alignas(64) AtomicBucket {
-    std::atomic<int> count;
-    char padding[64 - sizeof(std::atomic<int>)];
-    
-    AtomicBucket() : count(0) {}
-    
+struct alignas(64) Bucket {
+    int count;
+    std::mutex mtx;
+    Bucket() : count(0) {}
     void add(int delta) {
-        count.fetch_add(delta, std::memory_order_relaxed);
+        std::lock_guard<std::mutex> lock(mtx);
+        count += delta;
     }
 };
 
 struct histogram {
-    std::vector<AtomicBucket> bins;
-    
-    histogram(int count) : bins(count) { }
-
-    void add(int i) {
-        bins[i].add(1);
-    }
-    
-    void add_batch(int i, int delta) {
-        bins[i].add(delta);
-    }
-
-    int get(int i) const {
-        return bins[i].count.load(std::memory_order_relaxed);
-    }
-
-    void print_total(std::ostream& str) {
+    std::vector<Bucket> bins;
+    histogram(int count) : bins(count) {}
+    void add_batch(int i, int delta) { bins[i].add(delta); }
+    void print_total(ostream& str) {
         int total = 0;
-        for (const auto& bin : bins) {
-            total += bin.count.load(std::memory_order_relaxed);
-        }
+        for (auto& bin : bins) total += bin.count;
         str << "total:" << total << "\n";
     }
-
-    void print_bins(std::ostream& str) {
-        for (size_t i = 0; i < bins.size(); ++i) {
-            str << i << ":" << bins[i].count.load(std::memory_order_relaxed) << "\n";
-        }
+    void print_bins(ostream& str) {
+        for (size_t i = 0; i < bins.size(); ++i)
+            str << i << ":" << bins[i].count << "\n";
     }
 };
 
@@ -58,7 +40,6 @@ void worker(std::atomic<int>& counter, int sample_count, histogram& h, int gen_r
     generator gen(gen_range);
     const int CHUNK = 1'000'000;
     int local_counts[2] = {0, 0};
-    
     for (;;) {
         int start = counter.fetch_add(CHUNK, std::memory_order_relaxed);
         if (start >= sample_count) break;
@@ -75,34 +56,28 @@ void worker(std::atomic<int>& counter, int sample_count, histogram& h, int gen_r
 }
 
 int main(int argc, char **argv) {
-    int num_bins = 2;
-    int sample_count = 30000000;
+    int num_bins = 2, sample_count = 30000000;
     int num_threads = std::thread::hardware_concurrency();
     if (num_threads <= 0) num_threads = 1;
     int print_level = 3;
-    
     parse_args(argc, argv, num_threads, num_bins, sample_count, print_level);
-    num_bins = 2; // принудительно два бина
-    
+    num_bins = 2;
+
     histogram h(num_bins);
     std::atomic<int> counter{0};
     std::vector<std::thread> threads;
     threads.reserve(num_threads);
-    
+
     auto t1 = chrono::high_resolution_clock::now();
-    
-    for (int i = 0; i < num_threads; ++i) {
+    for (int i = 0; i < num_threads; ++i)
         threads.emplace_back(worker, std::ref(counter), sample_count, std::ref(h), 10);
-    }
-    
-    for (auto& th : threads) {
-        th.join();
-    }
-    
+    for (auto& th : threads) th.join();
     auto t2 = chrono::high_resolution_clock::now();
-    
-    if (print_level >= 0) cout << "Bins: " << num_bins << ", sample size: " << sample_count << ", threads: " << num_threads << endl;
+
+    if (print_level >= 0)
+        cout << "Bins: " << num_bins << ", sample size: " << sample_count << ", threads: " << num_threads << endl;
     if (print_level >= 3) h.print_bins(cout);
     if (print_level >= 1) h.print_total(cout);
-    if (print_level >= 2 || print_level == -1) cout << chrono::duration<double>(t2 - t1).count() << endl;
+    if (print_level >= 2 || print_level == -1)
+        cout << chrono::duration<double>(t2 - t1).count() << endl;
 }
